fbb7d93998fab42d9d68b5fc141a50164c96b19a
Shadid123 <shadidmazumder123@gmail.com>
2025-03-03 20:06:31 +0600

    Day-4 of C programming

---
+++ Day-4/Untitled1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    float base,height;
+    float area;
+    printf("Enter Base=");
+    scanf("%f",&base);
+    printf("Enter height=");
+     scanf("%f",&height);
+     area=1/2* base *height;
+     printf("Area=%.2f\n",area);
+
+     return 0;
+
+}
