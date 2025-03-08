b9517a173c75006fc7fb566e6640f9687ea7561e
Shadid Mazumder <shadidmazumder123@gmail.com>
2025-03-09 00:22:54 +0600

    Day-9 of C programming

---
+++ Day-9/Uppercase to lowercase.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+int main()
+{
+    char upper;
+    printf("Enter an uppercase letter:");
+    scanf("%c",&upper);//A=65
+
+    printf("The lower case letter is :%c",upper+32);//a=97
+
+    return 0;
+
+
+}
