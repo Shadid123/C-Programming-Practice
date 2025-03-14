046660bee99c3c9dc477f8916bf6f62619641674
Shadid Mazumder <shadidmazumder123@gmail.com>
2025-03-15 00:10:43 +0600

    Day-12 of C programming

---
+++ Day-12/Decimal to hexa.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+ int number;
+ printf("Enter a decimal number=");
+ scanf("%d",&number);
+
+ printf("Your hexadecimal number is=%x",number);
+ //scanf("%x",&number);
+
+
+
+
+return 0;
+
+
+}
