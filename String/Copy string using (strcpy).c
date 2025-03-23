8501853961946cc79cb7073136ffaa88719da856
Shadid Mazumder <shadidmazumder123@gmail.com>
2025-03-23 14:41:24 +0600

    Added a C program to copy a string using strcpy

---
+++ String/Copy string using (strcpy).c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main (){
+
+char Source[]="Shadid Mazumder";
+char Target[]={};
+strcpy(Target,Source);
+printf("Source=%s\n",Source);
+printf("Target=%s",Target);
+return 0 ;
+}
