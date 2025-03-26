58e375b15e6346d5632f7d566b9a5670aba85df3
Shadid Mazumder <shadidmazumder123@gmail.com>
2025-03-26 19:40:50 +0600

    Added C program to print each character of a string on a new line

---
+++ String/String lower case--uppercase.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+    char name[]="shadid";
+    strupr(name);
+    printf("%s\n",name);
+
+    char name1[]="SHADID";
+    strlwr(name1);
+    printf("%s",name1);
+
+return 0;
+
+}
